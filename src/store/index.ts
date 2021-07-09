b63b3c4dd5cac98d8003d97e0641ecbb9e9f4ed2
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-07-09 20:01:31 +0700

    realize store CRUD and first simple item component

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,76 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+
+import Note from "@/models/NoteModel";
+import ToDo from "@/models/ToDoModel";
 
 export default createStore({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
+    notes: [] as Note[],
+    currentNote: {
+      title: '',
+      todos: [] as ToDo[],
+      id: 0,
+    } as Note
   },
   mutations: {
+    addNote(state) {
+      state.notes.push(state.currentNote);
+    },
+    deleteNote(state) {
+      state.notes = state.notes.filter(note => note.id !== state.currentNote.id);
+    },
+    updateNote(state) {
+      const note = state.notes.find(note => note.id === state.currentNote.id);
+      const index = state.notes.indexOf(note as Note);
+      state.notes[index] = state.currentNote;
+    },
+    setCurrentNote(state, payload: Note) {
+      state.currentNote = payload;
+    },
+    updateTitles(state, payload: string) {
+      state.currentNote.title = payload;
+    },
+    updateTodos(state, payload: ToDo[]) {
+      state.currentNote.todos = payload;
+    },
+    addNewTodo(state) {
+      state.currentNote.todos.push({
+        text: '',
+        completed: false
+      });
+    },
+    deleteTodo(state, index: number) {
+      state.currentNote.todos.splice(index, 1);
+    },
   },
   actions: {
+    saveNote({commit}) {
+      const isOldNote: boolean = this.state.notes.some(note => note.id === this.state.currentNote.id);
+
+      if (isOldNote) {
+        commit('updateNote')
+      }
+      else {
+        commit('addNote');
+      }
+    },
+    fetchCurrentNote({commit}, noteId: number) {
+      const note = JSON.parse(JSON.stringify(this.state.notes.find(note => note.id === noteId)));
+      commit('setCurrentNote', note);
+    },
+    updateCurrentNote({commit}, note: Note) {
+      commit('setCurrentNote', note);
+    }
   },
-  modules: {
+  getters: {
+    getIdOfLastNote(state): number {
+      if (state.notes.length === 0) {
+        return 0;
+      }
+
+      const index = state.notes.length - 1;
+      return state.notes[index].id;
+    }
   }
 })
