a0af04be716f9efb951928ed3110244ae539abc3
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-07-10 02:21:49 +0700

    add Note component and start routing

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <div />
+  <div id="app">
+    <nav>
+      <router-link class="router-link" to="/" exact>List Of Notes</router-link>
+      <router-link class="router-link" to="/note" exact
+      >Create Note
+      </router-link
+      >
+    </nav>
+    <hr/>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
